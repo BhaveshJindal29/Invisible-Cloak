15b2c7a31873e2c85666f7f591b72a9e42b28805
Bhavesh Jindal <63338442+BhaveshJindal29@users.noreply.github.com>
2021-09-02 17:58:21 +0530

    Update Image_Capture.py

--- Image_Capture.py
+++ Image_Capture.py
@@ -1,1 +1,18 @@
+#this file captures the background image and saves it on the local system\
+#the image captured by this program will used in the file "invisible_cloak.py"
+
+import cv2
+
+cap = cv2.VideoCapture(0)     #starting camera
+
+while cap.isOpened():
+    ret,background =  cap.read()
+    if ret:
+        cv2.imshow("Capturing Background Image",background)
+        if cv2.waitKey(1) == ord('q'):                                #when user presses 'q' the captured image will be saved
+            cv2.imwrite("image.jpg",background)
+            break
+
+cap.release()
+cv2.destroyAllWindows()
 
